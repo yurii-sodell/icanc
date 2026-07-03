3ea1eb4352e85153f4d307b6842484fd977a0f1b
Sodel Yurii <77705770+yurii-sodell@users.noreply.github.com>
2026-07-03 21:29:51 +0200

    icanc.sh 1.0
    - Added supported formats check. You can add mannualy more compiles' calls and formats in case you needed it.

--- icanc.sh
+++ icanc.sh
@@ -15,7 +15,7 @@
 #   ./icanc.sh -p=./my_project_folder -f=.cpp -r
 
 print_help() {
-	cat <<EOF
+        cat <<HELP
 icanc compiles all C/C++ files in a folder into a single binary
 
 Usage:
@@ -29,56 +29,76 @@ Options:
 
 Example:
   $(basename "$0") -p=./my_project -f=.cpp -r
-EOF
+HELP
 }
 
 if [ $# -eq 0 ]; then
-	echo "Executing folder was not specified. Use -f=folder_name";
-	exit 1;
+        echo "Executing folder was not specified. Use -p=folder_name";
+        exit 1;
 fi;
 
 if [[ "$1" == "-h" || "$1" == "--help" ]]; then
-	print_help;
-	exit 0;
+        print_help;
+        exit 0;
 fi;
 
 if [[ "$1" != -p=* ]]; then
-	echo "-p=path expected as a first argument";
-	print_help;
-	exit 2;
+        echo "-p=path expected as a first argument";
+        print_help;
+        exit 2;
 fi;
+
+
+SUPPORTED_FORMATS=(".c" ".cpp"); #you can adjust it in case you would like to provide other compilers
+        #don't forget to add call at the end
 
 RUN_AT_THE_END_FLAG=0;
 FOLDER="";
 FORMAT=".c";
 
 for arg in "$@"; do
-	case $arg in
-		-p=*)
-			FOLDER=$(echo "${arg}" | sed 's/^-p=//');
-			;;
-		-f=*)
-			FORMAT=$(echo "${arg}" | sed 's/^-f=//');
-			;;
-		-r)
-			RUN_AT_THE_END_FLAG=1;
-			;;
-		-h|--help)
-			print_help;
-			exit 0;
-			;;
-		*)
-			echo "Cannot recognize ${arg} parameter";
-			exit 3;
-			;;
-	esac;
+        case $arg in
+                -p=*)
+                        FOLDER=$(echo "${arg}" | sed 's/^-p=//');
+                        ;;
+                -f=*)
+                        FORMAT=$(echo "${arg}" | sed 's/^-f=//');
+                        ;;
+                -r)
+                        RUN_AT_THE_END_FLAG=1;
+                        ;;
+                -h|--help)
+                        print_help;
+                        exit 0;
+                        ;;
+                *)
+                        echo "Cannot recognize ${arg} parameter";
+                        exit 3;
+                        ;;
+        esac;
 done;
+
+
+function isSupported() {
+for form in "${SUPPORTED_FORMATS[@]}"; do
+        if [[ "$FORMAT" == "$form" ]]; then 
+                return 0;
+        fi
+done;
+return 1;
+}
+
+isSupported;
+if [ $? -eq 1 ]; then
+        echo "Provided format ${FORMAT} is not supported";
+        exit 4;
+fi
 
 FOLDER="${FOLDER%/}";
 
 if [ ! -d "$FOLDER" ]; then
-	echo "Folder '${FOLDER}' does not exist";
-	exit 5;
+        echo "Folder '${FOLDER}' does not exist";
+        exit 5;
 fi;
 
 printf "\n#####################"
@@ -87,45 +107,50 @@ printf "#####################\n"
 
 FILES_TO_RUN=();
 while read -r filepath; do
-	FILES_TO_RUN+=("$filepath");
+        FILES_TO_RUN+=("$filepath");
 done < <(find "$FOLDER" -type f -name "*${FORMAT}"); # to provide format
 
 if [ ${#FILES_TO_RUN[@]} -eq 0 ]; then
-	echo "No files with provided format (${FORMAT}) were found";
-	exit 4;
+        echo "No files with provided format (${FORMAT}) were found";
+        exit 4;
 fi
 
 printf "\n#####################"
 printf "\nFollowing files are going to be compiled: \n";
 for element in "${FILES_TO_RUN[@]}"; do
-	printf "%s\n" "$element";
+        printf "%s\n" "$element";
 done;
 printf "#####################\n"
 
 if [ ! -d "$FOLDER/output" ]; then
-	mkdir "$FOLDER/output";
+        mkdir "$FOLDER/output";
 fi
 
 COMPILED_PROGRAM="$FOLDER/output/program";
 
-if [ "${FORMAT}" == ".c" ]; then
-	gcc "${FILES_TO_RUN[@]}" -o "$COMPILED_PROGRAM";
-elif [ "$FORMAT" == ".cpp" ]; then
-	g++ "${FILES_TO_RUN[@]}" -o "$COMPILED_PROGRAM";
-else
-	echo "Unsupported format: ${FORMAT} (only .c and .cpp are supported)";
-	exit 6;
-fi
+
+case $FORMAT in
+        ".c")
+                gcc "${FILES_TO_RUN[@]}" -o "$COMPILED_PROGRAM";
+        ;;
+        ".cpp")
+                g++ "${FILES_TO_RUN[@]}" -o "$COMPILED_PROGRAM";
+        ;;
+        *)
+                echo "Unsupported format: ${FORMAT} (only .c and .cpp are supported)";
+                exit 6;
+        ;;
+esac
 
 BUILD_STATUS=$?;
 if [ $BUILD_STATUS -ne 0 ]; then
-	echo "Build failed";
-	exit $BUILD_STATUS;
+        echo "Build failed";
+        exit $BUILD_STATUS;
 fi
 
 #Windows and git bash appends .exe automatically, otherwise it will be appended here:
 if [ -f "${COMPILED_PROGRAM}.exe" ]; then
-	COMPILED_PROGRAM="${COMPILED_PROGRAM}.exe";
+        COMPILED_PROGRAM="${COMPILED_PROGRAM}.exe";
 fi
 
 printf "\n#####################";
@@ -133,9 +158,8 @@ printf '\nSuccessfully compiled and stored at %s' "${COMPILED_PROGRAM}";
 printf "\n#####################\n";
 
 if [ $RUN_AT_THE_END_FLAG -eq 1 ]; then
-	printf "\n#####################";
-	printf '\nStarting program at %s' "${COMPILED_PROGRAM}";
-	printf "\n#####################\n\n";
-	"$COMPILED_PROGRAM";
+        printf "\n#####################";
+        printf '\nStarting program at %s' "${COMPILED_PROGRAM}";
+        printf "\n#####################\n\n";
+        "$COMPILED_PROGRAM";
 fi
-
